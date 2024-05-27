b42d1e400908a9c6661bfdd7523cc0a68283585e
MarinaRaskevich <marina.raskevich19@gmail.com>
2024-05-27 16:36:40 +0200

    config changes

---
+++ www/config/config.php
@@ -0,0 +1,4 @@
+<?php
+define('POKEMON_TYPES', 'https://pokebuildapi.fr/api/v1/types');
+define('ONE_TYPE_POKEMONS', 'https://pokebuildapi.fr/api/v1/pokemon/type/');
+define('POKEMON_INFORMATION', 'https://pokebuildapi.fr/api/v1/pokemon/');
