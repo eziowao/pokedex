7b4284ce25b89cd0b10e53839ad5f02783e6ef2d
christopher <christopher80090@hotmail.fr>
2024-05-29 10:06:32 +0200

    ajout gestion des erreurs

--- www/controllers/pokemonListController.php
+++ www/controllers/pokemonListController.php
@@ -3,7 +3,11 @@ require_once __DIR__ . '/../models/Pokemon.php';
 
 //???
 try {
-    $type = $_GET['type'];
+    if (array_key_exists('type', $_GET) && $_GET['type'] != null) {
+        $type = $_GET['type'];
+    }else {
+        $errors['pokemons'] = 'Type de pokémons non sélectionner';
+    }
     if (allPokemonsOfSameType($type)) {
         $listOfPokemons = allPokemonsOfSameType($type);
     } else {
@@ -11,10 +15,13 @@ try {
     }
 } catch (Exception $e) {
     $errors = $e->getMessage();
-    include __DIR__ . '/../404.php';
 }
 
 
 // include __DIR__ . '/../views/templates/header.php';
-include __DIR__ . '/../views/pokemon/list.php';
+if (empty($errors)) {
+    include __DIR__ . '/../views/pokemon/list.php';
+}else{
+    include __DIR__ . '/../404.php';
+}
 // include __DIR__ . '/../views/templates/footer.php';
