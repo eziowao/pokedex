4e4c1a7f97fc7e073a088e1e59c89f80103fdb35
Wav <elzwo80@gmail.com>
2024-05-29 11:54:40 +0200

    navbar hidden

--- www/views/templates/header.php
+++ www/views/templates/header.php
@@ -1,12 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/assets/css/style.css">
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/8049f56234.js" crossorigin="anonymous"></script>
     <title>Accueil</title>
 </head>
 
 <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-purple justify-content-center">
+            <a class="navbar-brand text-center" href="#"><img src="./../../public/assets/img/pokedex_logo.png" class="h-50 w-50" alt="logo pokedex avec un ectoplasma"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav justify-content-around w-100 d-flex">
+                    <li class="nav-item text-center">
+                        <a class="nav-link fs-2" href="#">Accueil <i class="fa-solid fa-house ms-4"></i></a>
+                    </li>
+                    <li class="nav-item text-center">
+                        <a class="nav-link fs-2" href="#">Favoris <i class="fa-solid fa-star ms-4"></i></a>
+                    </li>
+                    <li class="nav-item text-center">
+                        <a class="nav-link fs-2" href="#">Paramètres <i class="fa-solid fa-cog ms-4"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- <div class="bg-purple pb-3">
+            <form class="row align-items-center justify-content-center">
+                <div class="col-10 col-lg-5">
+                    <input class="form-control rounded-5" type="search" placeholder="Bulbizarre ou #001" aria-label="Rechercher">
+                </div>
+            </form>
+        </div> -->
+    </header>
