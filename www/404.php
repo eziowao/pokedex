769ac2f2ed2253f97ce7b8dd9b46adefdda44419
Wav <elzwo80@gmail.com>
2024-05-30 15:14:34 +0200

    delete includes val

--- www/404.php
+++ www/404.php
@@ -1,17 +1,8 @@
 <!-- Une fois la gestion d'erreure terminée, enlever les includes... -->
 
-<?php
-include __DIR__ . '/views/templates/header.php'
-?>
 
 <div class="container text-center">
     <h1 class="display-4 mt-5">Erreur 404</h1>
     <img src="./public/assets/img/error404pkmn.png" alt="image d'erreur" class="img-fluid my-4 w-25 h-25">
     <p class="lead fs-2">La page que vous cherchez n'existe pas.</p>
 </div>
-
-<?php
-
-include __DIR__ . '/views/templates/footer.php'
-
-?>
